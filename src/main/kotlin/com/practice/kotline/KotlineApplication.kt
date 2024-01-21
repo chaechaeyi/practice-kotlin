e10bb938e3cebdbd2effa9322398659002820ec1
cgyi29 <cgyi29@tmobility.com>
2024-01-21 17:19:42 +0900

    first commit

---
+++ src/main/kotlin/com/practice/kotline/KotlineApplication.kt
@@ -0,0 +1,11 @@
+package com.practice.kotline
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlineApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlineApplication>(*args)
+}
---
+++ src/test/kotlin/com/practice/kotline/KotlineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.practice.kotline
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
